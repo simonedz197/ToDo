01f9528b329f48467ac3d480bb061dc6a80bf308
simonedz197 <simon.edmundson@bjss.com>
2025-05-08 14:23:15 +0100

    update cli for user

--- todo_cli.go
+++ todo_cli.go
@@ -9,6 +9,7 @@ import (
 	list "tut2/todo/todolist"
 )
 
+var uidFlag = flag.String("uid", "", "owner of the todo list e.g. -uid simon")
 var addFlag = flag.String("add", "", "add the todo list entry e.g. -add \"buy milk\"")
 var updateFlag = flag.String("update", "", "update the todo list entry e.g. -update \"buy milk\" \"buy 2 pints of milk\"")
 var deleteFlag = flag.String("delete", "", "delete the todo list entry e.g. -delete \"buy milk\"\nUse delete \"*\" to delete all")
@@ -42,6 +43,7 @@ func main() {
 	// setup a dummy context
 	// we should get this passed in eventually
 	ctx := dummyContext()
+	uid := "Anonymous User"
 
 	// start the job queue prcessor
 	go list.ProcessDataJobs()
@@ -49,13 +51,14 @@ func main() {
 	flag.Parse()
 
 	flagsSet := flagsPassed()
+	uid = *uidFlag
 
-	if len(flagsSet) > 1 {
+	if len(flagsSet) > 2 {
 		list.Logger.ErrorContext(ctx, "Error parsing command line", "details", "too many flags passed")
 		return
 	}
 	// load data
-	data := list.DataStoreJob{ctx, list.LoadData, "", "", make(chan list.ReturnChannelData)}
+	data := list.DataStoreJob{ctx, "", list.LoadData, "", "", make(chan list.ReturnChannelData)}
 	list.DataJobQueue <- data
 	returnVal, ok := <-data.ReturnChannel
 	if ok {
@@ -68,7 +71,7 @@ func main() {
 	// save data deferred to last thing to do
 	defer func() {
 		fmt.Printf("\nclosing down...\n")
-		data := list.DataStoreJob{ctx, list.StoreData, "", "", make(chan list.ReturnChannelData)}
+		data := list.DataStoreJob{ctx, "", list.StoreData, "", "", make(chan list.ReturnChannelData)}
 		list.DataJobQueue <- data
 		returnVal, ok := <-data.ReturnChannel
 		if ok {
@@ -81,7 +84,7 @@ func main() {
 
 	switch flagsSet[0] {
 	case "add":
-		data := list.DataStoreJob{ctx, list.AddData, *addFlag, "", make(chan list.ReturnChannelData)}
+		data := list.DataStoreJob{ctx, uid, list.AddData, *addFlag, "", make(chan list.ReturnChannelData)}
 		list.DataJobQueue <- data
 		returnVal, ok := <-data.ReturnChannel
 		if ok {
@@ -91,7 +94,7 @@ func main() {
 			}
 		}
 	case "delete":
-		data := list.DataStoreJob{ctx, list.DeleteData, *deleteFlag, "", make(chan list.ReturnChannelData)}
+		data := list.DataStoreJob{ctx, uid, list.DeleteData, *deleteFlag, "", make(chan list.ReturnChannelData)}
 		list.DataJobQueue <- data
 		returnVal, ok := <-data.ReturnChannel
 		if ok {
@@ -104,7 +107,7 @@ func main() {
 		if flag.NArg() == 0 {
 			fmt.Printf("\nyou need to enter the value to update to")
 		}
-		data := list.DataStoreJob{ctx, list.UpdateData, *updateFlag, flag.Arg(0), make(chan list.ReturnChannelData)}
+		data := list.DataStoreJob{ctx, uid, list.UpdateData, *updateFlag, flag.Arg(0), make(chan list.ReturnChannelData)}
 		list.DataJobQueue <- data
 		returnVal, ok := <-data.ReturnChannel
 		if ok {
@@ -114,7 +117,7 @@ func main() {
 			}
 		}
 	}
-	data = list.DataStoreJob{ctx, list.FetchData, "", "", make(chan list.ReturnChannelData)}
+	data = list.DataStoreJob{ctx, uid, list.FetchData, "", "", make(chan list.ReturnChannelData)}
 	list.DataJobQueue <- data
 	returnVal, ok = <-data.ReturnChannel
 	if ok {
